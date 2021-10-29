75331c139e4580237532a5c2c79e9f0d96c4e4a1
Rahul yadav <ry1091997@gmail.comv>
2021-10-29 16:05:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Create from "./Components/Create";
+import Header from "./Components/Header";
+import Add from "./Components/Add";
+
+toast.configure();
 
 function App() {
+  const [userData, setUserData] = useState([]);
+  let [check, setCheck] = useState(false);
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users/").then((result) => {
+      result.json().then((resp) => {
+        // console.log(resp);
+        setUserData(resp.data);
+      });
+    });
+  }, []);
+
+  function upDate(id) {
+    return userData[id];
+  }
+
+  const notification = (rah) => toast(rah);
+
+  function dataUpdate(ob, id) {
+    setUserData((prevItem) => {
+      return prevItem.map((item, index) => {
+        if (index !== id) {
+          return item;
+        } else {
+          item.first_name = ob.first_name;
+          item.last_name = ob.last_name;
+          item.email = ob.email;
+          return item;
+        }
+      });
+      // return { ...prevItem, ob };
+    });
+  }
+  function deleteUser(indexId) {
+    setUserData((prevValue) => {
+      return prevValue.filter((item, index) => {
+        return index !== indexId;
+      });
+    });
+  }
+
+  function addNewUser(user) {
+    setUserData((prevItem) => {
+      return [...prevItem, user];
+    });
+  }
+
+  function condToggle() {
+    check = check === false ? true : false;
+    setCheck(check);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header condToggle={condToggle} />
+      {check ? <Add addUser={addNewUser} notification={notification} /> : null}
+      <div className="grid">
+        {userData.map((item, id) => {
+          return (
+            <Create
+              key={id}
+              id={id}
+              email={item.email}
+              l_name={item.last_name}
+              f_name={item.first_name}
+              img={item.avatar}
+              find={upDate}
+              update={dataUpdate}
+              deleteUser={deleteUser}
+              notification={notification}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
